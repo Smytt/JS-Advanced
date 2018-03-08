66ba0fad6baf6d2c373ec6bbacf06e24e6588526
Smytt <parushev.kiril@gmail.com>
2018-03-08 19:39:46 +0200

    unit exercise

--- 11. Unit Testing - Exercise/tests/armageDom.js
+++ 11. Unit Testing - Exercise/tests/armageDom.js
@@ -2,7 +2,14 @@ let expect = require("chai").expect
 let jsdom = require('jsdom-global')()
 let $ = require('jquery')
 
-let html =`<div id="target">
+
+document.body.innerHTML = html
+
+let nuke = require('../06. ArmageDOM')
+
+
+describe('Test armageDom', () => {
+    let html = `<div id="target">
     <div class="nested target">
         <p>This is some text</p>
     </div>
@@ -15,18 +22,34 @@ let html =`<div id="target">
     </div>
 </div>`
 
-document.body.innerHTML = html
-
-let nuke = require('../06. ArmageDOM')
-
-
-describe('Test armageDom', () => {
     beforeEach(() => {
         document.body.innerHTML = html
     })
+
+    it('should do nothing for {target}', () => {
+        nuke('.target')
+        expect(document.body.innerHTML).to.equal(html)
+    })
+    it('should do nothing for {}', () => {
+        nuke()
+        expect(document.body.innerHTML).to.equal(html)
+    })
+    it('should do nothing for {5,10}', () => {
+        nuke(5, 10)
+        expect(document.body.innerHTML).to.equal(html)
+    })
+    it('should do nothing for {inside, inside}', () => {
+        nuke('.inside', '.inside')
+        expect(document.body.innerHTML).to.equal(html)
+    })
+
     it('should delete {nested target}', () => {
         nuke('.nested', '.target')
         expect($('.nested.target').length).to.equal(0)
+    })
+    it('should delete {nested + target}', () => {
+        nuke('.nested', '.target')
+        expect($('.nested').filter('.target').length).to.equal(0)
     })
     it('should not delete {nested}', () => {
         nuke('.nested', '.target')
